6bddae4f7e979783c992e8efcf0e577cc450b759
personality173 <125491478+personality173@users.noreply.github.com>
2023-03-09 06:59:26 +0300

    Add files via upload

---
+++ lab4math.py
@@ -0,0 +1,22 @@
+
+import math
+#1-exercise
+degree = float(input())
+radian = degree * (math.pi / 180)
+print(radian)
+# 2 - exercise
+height = float(input())
+a = float(input())
+b = float(input())
+S = ((a+b) / 2) * height
+print(S)
+#3-exercise
+n = int(input())
+l = float(input())
+S = ((l**2) * n) / 4 * math.tan(math.pi/n)
+print(S)
+#4-exercise
+b = float(input())
+h = float(input())
+S = b * h
+print(S)
